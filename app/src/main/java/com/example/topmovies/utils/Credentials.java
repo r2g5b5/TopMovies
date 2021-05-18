418ff2a3624e0051e3489aced27fd076e85dfd9f
r2g5b5 <programmer4906@gmail.com>
2021-05-18 21:03:42 +0430

    Initial commit

---
+++ app/src/main/java/com/example/topmovies/utils/Credentials.java
@@ -0,0 +1,8 @@
+package com.example.topmovies.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL="https://api.themoviedb.org";
+
+    public static final String API_KEY="https://api.themoviedb.org";
+}
